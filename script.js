0cad81a511243c312b1d6824618b5ba5cc2edbb5
andromeda <q.a.r.h.hrp0204@gmail.com>
2025-10-28 21:34:22 +0700

    Update 1.1

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+
 function DigitalOnline() {
     localStorage.setItem("courseTitle", "Digital Online Marketing");
     localStorage.setItem("courseBanner", "../assets/images/digital-online.png");
+    localStorage.setItem(
+        "courseDescription",
+        "Peralihan dari iklan tradisional ke digital tidak hanya meningkatkan intensitas kampanye, tetapi juga membuat kinerjanya lebih terukur. <br><br>Digital marketing menjadi krusial karena masyarakat modern sebagian besar waktunya dihabaskan di internet. Hal ini membuka peluang bagi bisnis untuk menjangkau audiens yang lebih luas dan mempromosikan produk secara efektif. <br><br>Lebih dari itu, digital marketing menawarkan efisiensi biaya dan cara yang lebih ekonomis dibandingkan metode periklanan konvensional."
+    );
+    localStorage.setItem(
+        "courseSylabus",
+        "Dilengkapi dengan video tutorial, praktik, dan kuis, berikut adalah topik pembahasan yang akan dipelajari: <br><br><strong>1. Digital Marketing Fundamental</strong> <br>Pada topik ini, kamu akan mempelajari konsep dasar atau fundamental yang perlu kamu ketahui dan pahami untuk menjadi seorang Digital Marketing Specialist. Dimulai dari pembahasan tentang bagaimana digital marketing di kondisi saat ini, tipe-tipe digital marketing dan bagaimana tipe digital marketing ini bekerja, juga tentang seberapa penting sebuah riset market dan implementasinya. <br><br><strong>2. Know The Product</strong> <br>Berikutnya, kamu akan mempelajari tentang pentingnya menguasai produk atau layanan yang akan dijual untuk kesuksesan sebuah campaign. Pada materi ini, akan dibahas tentang Unique Selling Proposition (USP) dan bagaimana membuatnya; perbedaan Benefit-Feature-Guarantee (BFG) dan implementasinya pada sebuah iklan; juga tentang Offers yang akan dibahas berdasarkan contoh produk tertentu. <br><br><strong>3. Tantangan Membantu UMKM</strong> <br>Terakhir, yang tidak kalah menarik adalah tantangan untuk membantu UMKM yang telah bermitra dengan Jobskill di sekitar Anda. Anda akan mendapat ulasan dari UMKM yang dibantu, waktu pengerjaan 2 minggu, dan upah setelah berhasil menyelesaikan tantangan ini."
+    )
+    localStorage.setItem("totalMateri", "21 materi");
 }
 
 function SosmedMarketing() {
     localStorage.setItem("courseTitle", "Social Media Marketing");
     localStorage.setItem("courseBanner", "../assets/images/sosmed-marketing.png");
+    localStorage.setItem(
+        "courseDescription",
+        "Peralihan dari iklan tradisional ke digital tidak hanya meningkatkan intensitas kampanye, tetapi juga membuat kinerjanya lebih terukur. <br><br>Digital marketing menjadi krusial karena masyarakat modern sebagian besar waktunya dihabaskan di internet. Hal ini membuka peluang bagi bisnis untuk menjangkau audiens yang lebih luas dan mempromosikan produk secara efektif. <br><br>Lebih dari itu, digital marketing menawarkan efisiensi biaya dan cara yang lebih ekonomis dibandingkan metode periklanan konvensional."
+    );
+    localStorage.setItem(
+        "courseSylabus",
+        "Dilengkapi dengan video tutorial, praktik, dan kuis, berikut adalah topik pembahasan yang akan dipelajari: <br><br><strong>1. Digital Marketing Fundamental</strong> <br>Pada topik ini, kamu akan mempelajari konsep dasar atau fundamental yang perlu kamu ketahui dan pahami untuk menjadi seorang Digital Marketing Specialist. Dimulai dari pembahasan tentang bagaimana digital marketing di kondisi saat ini, tipe-tipe digital marketing dan bagaimana tipe digital marketing ini bekerja, juga tentang seberapa penting sebuah riset market dan implementasinya. <br><br><strong>2. Know The Product</strong> <br>Berikutnya, kamu akan mempelajari tentang pentingnya menguasai produk atau layanan yang akan dijual untuk kesuksesan sebuah campaign. Pada materi ini, akan dibahas tentang Unique Selling Proposition (USP) dan bagaimana membuatnya; perbedaan Benefit-Feature-Guarantee (BFG) dan implementasinya pada sebuah iklan; juga tentang Offers yang akan dibahas berdasarkan contoh produk tertentu. <br><br><strong>3. Tantangan Membantu UMKM</strong> <br>Terakhir, yang tidak kalah menarik adalah tantangan untuk membantu UMKM yang telah bermitra dengan Jobskill di sekitar Anda. Anda akan mendapat ulasan dari UMKM yang dibantu, waktu pengerjaan 2 minggu, dan upah setelah berhasil menyelesaikan tantangan ini."
+    )
+    localStorage.setItem("totalMateri", "24 materi");
 }
 
 function MarketingOffice() {
     localStorage.setItem("courseTitle", "Digital Online Marketing");
     localStorage.setItem("courseBanner", "../assets/images/marketing-office.png");
+    localStorage.setItem(
+        "courseDescription",
+        "Peralihan dari iklan tradisional ke digital tidak hanya meningkatkan intensitas kampanye, tetapi juga membuat kinerjanya lebih terukur. <br><br>Digital marketing menjadi krusial karena masyarakat modern sebagian besar waktunya dihabaskan di internet. Hal ini membuka peluang bagi bisnis untuk menjangkau audiens yang lebih luas dan mempromosikan produk secara efektif. <br><br>Lebih dari itu, digital marketing menawarkan efisiensi biaya dan cara yang lebih ekonomis dibandingkan metode periklanan konvensional."
+    );
+    localStorage.setItem(
+        "courseSylabus",
+        "Dilengkapi dengan video tutorial, praktik, dan kuis, berikut adalah topik pembahasan yang akan dipelajari: <br><br><strong>1. Digital Marketing Fundamental</strong> <br>Pada topik ini, kamu akan mempelajari konsep dasar atau fundamental yang perlu kamu ketahui dan pahami untuk menjadi seorang Digital Marketing Specialist. Dimulai dari pembahasan tentang bagaimana digital marketing di kondisi saat ini, tipe-tipe digital marketing dan bagaimana tipe digital marketing ini bekerja, juga tentang seberapa penting sebuah riset market dan implementasinya. <br><br><strong>2. Know The Product</strong> <br>Berikutnya, kamu akan mempelajari tentang pentingnya menguasai produk atau layanan yang akan dijual untuk kesuksesan sebuah campaign. Pada materi ini, akan dibahas tentang Unique Selling Proposition (USP) dan bagaimana membuatnya; perbedaan Benefit-Feature-Guarantee (BFG) dan implementasinya pada sebuah iklan; juga tentang Offers yang akan dibahas berdasarkan contoh produk tertentu. <br><br><strong>3. Tantangan Membantu UMKM</strong> <br>Terakhir, yang tidak kalah menarik adalah tantangan untuk membantu UMKM yang telah bermitra dengan Jobskill di sekitar Anda. Anda akan mendapat ulasan dari UMKM yang dibantu, waktu pengerjaan 2 minggu, dan upah setelah berhasil menyelesaikan tantangan ini."
+    )
+    localStorage.setItem("totalMateri", "20 materi");
 }
 
 function contentLoaded() {
@@ -18,14 +46,17 @@ function contentLoaded() {
     const banner = document.getElementById("courseBanner");
     const desc = document.getElementById("courseDescription");
     const sylabus = document.getElementById("courseSylabus");
+    const total = document.getElementById("totalMateri");
 
     const savedTitle = localStorage.getItem("courseTitle");
     const savedBanner = localStorage.getItem("courseBanner");
     const savedDesc = localStorage.getItem("courseDescription");
     const savedSylabus = localStorage.getItem("courseSylabus");
+    const savedTotal = localStorage.getItem("totalMateri");
 
     title.innerText = savedTitle;
     banner.src = savedBanner;
-    desc.src = savedDesc;
-    sylabus.src = savedSylabus;
+    desc.innerHTML = savedDesc;
+    sylabus.innerHTML = savedSylabus;
+    total.innerHTML = savedTotal;
 }
