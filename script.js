d3edd03ef0fb396cf15601986f08e07aed5ed7f3
andromeda <q.a.r.h.hrp0204@gmail.com>
2025-10-29 10:06:34 +0700

    Bismillah Last 2.0

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ function DigitalOnline() {
         "Dilengkapi dengan video tutorial, praktik, dan kuis, berikut adalah topik pembahasan yang akan dipelajari: <br><br><strong>1. Digital Marketing Fundamental</strong> <br>Pada topik ini, kamu akan mempelajari konsep dasar atau fundamental yang perlu kamu ketahui dan pahami untuk menjadi seorang Digital Marketing Specialist. Dimulai dari pembahasan tentang bagaimana digital marketing di kondisi saat ini, tipe-tipe digital marketing dan bagaimana tipe digital marketing ini bekerja, juga tentang seberapa penting sebuah riset market dan implementasinya. <br><br><strong>2. Know The Product</strong> <br>Berikutnya, kamu akan mempelajari tentang pentingnya menguasai produk atau layanan yang akan dijual untuk kesuksesan sebuah campaign. Pada materi ini, akan dibahas tentang Unique Selling Proposition (USP) dan bagaimana membuatnya; perbedaan Benefit-Feature-Guarantee (BFG) dan implementasinya pada sebuah iklan; juga tentang Offers yang akan dibahas berdasarkan contoh produk tertentu. <br><br><strong>3. Tantangan Membantu UMKM</strong> <br>Terakhir, yang tidak kalah menarik adalah tantangan untuk membantu UMKM yang telah bermitra dengan Jobskill di sekitar Anda. Anda akan mendapat ulasan dari UMKM yang dibantu, waktu pengerjaan 2 minggu, dan upah setelah berhasil menyelesaikan tantangan ini."
     )
     localStorage.setItem("totalMateri", "21 materi");
+    localStorage.setItem("coursePoint", "1121");
+    localStorage.setItem("courseScore", "A+");
+    localStorage.setItem("maxPoint", "1320 max poin");
+
+    localStorage.setItem("courseImage1", "../assets/images/detail-satu.png");
+    localStorage.setItem("courseImage2", "../assets/images/detail-dua.png");
+    localStorage.setItem("courseImage3", "../assets/images/detail-tiga.png");
+    localStorage.setItem("courseImage4", "../assets/images/detail-empat.png");
 }
 
 function SosmedMarketing() {
@@ -25,6 +33,14 @@ function SosmedMarketing() {
         "Dilengkapi dengan video tutorial, praktik langsung, dan kuis interaktif, berikut adalah topik pembahasan yang akan dipelajari: <br><br><strong>1. Pengenalan Social Media Marketing</strong> <br>Pada topik ini, kamu akan mempelajari dasar-dasar dari pemasaran di media sosial, mulai dari perbedaan antar platform (Instagram, TikTok, dan lainnya), memahami audience behavior, serta cara menentukan goals dan key metrics dari setiap kampanye. Kamu juga akan dikenalkan pada strategi membangun brand awareness dan pentingnya content consistency. <br><br><strong>2. Strategi Konten & Copywriting</strong> <br>Di bagian ini, kamu akan belajar cara membuat konten yang menarik dan relevan dengan target pasar. Kamu akan memahami bagaimana membuat content plan, teknik copywriting yang persuasif, penggunaan call to action (CTA) yang efektif, serta cara menganalisis performa konten menggunakan insight tools. <br><br><strong>3. Project: Membantu UMKM Meningkatkan Engagement</strong> <br>Sebagai penutup, kamu akan mendapatkan tantangan untuk mengelola akun media sosial dari salah satu UMKM mitra JobSkill. Kamu akan membuat campaign kecil untuk meningkatkan engagement dan reach mereka. Dalam waktu 2 minggu, hasil kerja akan dinilai langsung oleh mentor, dan kamu berkesempatan mendapatkan bayaran sesuai performa terbaikmu."
     )
     localStorage.setItem("totalMateri", "24 materi");
+    localStorage.setItem("coursePoint", "1011");
+    localStorage.setItem("courseScore", "A-");
+    localStorage.setItem("maxPoint", "1500 max poin");
+
+    localStorage.setItem("courseImage1", "../assets/images/image_1.png");
+    localStorage.setItem("courseImage2", "../assets/images/image_2.png");
+    localStorage.setItem("courseImage3", "../assets/images/image_3.png");
+    localStorage.setItem("courseImage4", "../assets/images/image_4.png");
 }
 
 function MarketingOffice() {
@@ -39,6 +55,14 @@ function MarketingOffice() {
         "Dilengkapi dengan video tutorial, praktik, dan kuis, berikut adalah topik pembahasan yang akan dipelajari: <br><br><strong>Kamu akan mendalami konsep fundamental untuk dikuasai oleh tim Marketing Office dalam era digital. Pembahasan akan dimulai dari:</strong> <br><br><strong>1. Pondasi Pemasaran Digital</strong> <br>Riset Pasar & Implementasi: Pentingnya melakukan riset pasar yang mendalam dan cara mengaplikasikan data riset tersebut ke dalam strategi dan eksekusi kampanye pemasaran. <br><br><strong>2. Penguasaan Produk dan Layanan Kantor</strong> <br>Unique Selling Proposition (USP) dan Pengembangan Nilai Jual: Cara menemukan dan merumuskan keunggulan unik produk/layanan kantor Anda dibandingkan kompetitor. <br><br>Strategi Penawaran (Offers): Merancang penawaran yang menarik dan persuasif, dibahas melalui studi kasus spesifik produk atau layanan yang ditangani oleh kantor. <br><br><strong>3. Proyek Implementasi dan Dukungan UMKM</strong> <br>Tantangan Kemitraan UMKM: Mengerjakan proyek pemasaran untuk Usaha Mikro, Kecil, dan Menengah (UMKM) mitra kantor di wilayah terdekat Anda. <br>Sistem Pengerjaan: Proyek ini memiliki alokasi waktu 2 minggu untuk pengerjaan. <br><br>Evaluasi dan Penghargaan: Setelah berhasil menyelesaikan tantangan, Anda akan menerima ulasan dan feedback langsung dari UMKM yang dibantu, serta mendapatkan upah/insentif atas keberhasilan implementasi strategi pemasaran."
     )
     localStorage.setItem("totalMateri", "20 materi");
+    localStorage.setItem("coursePoint", "950");
+    localStorage.setItem("courseScore", "A+");
+    localStorage.setItem("maxPoint", "990 max poin");
+
+    localStorage.setItem("courseImage1", "../assets/images/2image_1.png");
+    localStorage.setItem("courseImage2", "../assets/images/2image_2.png");
+    localStorage.setItem("courseImage3", "../assets/images/2image_3.png");
+    localStorage.setItem("courseImage4", "../assets/images/2image_4.png");
 }
 
 function contentLoaded() {
@@ -46,17 +70,40 @@ function contentLoaded() {
     const banner = document.getElementById("courseBanner");
     const desc = document.getElementById("courseDescription");
     const sylabus = document.getElementById("courseSylabus");
+
     const total = document.getElementById("totalMateri");
+    const maxPoint = document.getElementById("maxPoint");
+    const point = document.getElementById("coursePoint");
+    const score = document.getElementById("courseScore");
+    const image1 = document.getElementById("courseImage1");
+    const image2 = document.getElementById("courseImage2");
+    const image3 = document.getElementById("courseImage3");
+    const image4 = document.getElementById("courseImage4");
 
     const savedTitle = localStorage.getItem("courseTitle");
     const savedBanner = localStorage.getItem("courseBanner");
     const savedDesc = localStorage.getItem("courseDescription");
     const savedSylabus = localStorage.getItem("courseSylabus");
+    
     const savedTotal = localStorage.getItem("totalMateri");
+    const savedMaxPoint = localStorage.getItem("maxPoint");
+    const savedPoint = localStorage.getItem("coursePoint");
+    const savedScore = localStorage.getItem("courseScore");
+    const savedImage1 = localStorage.getItem("courseImage1");
+    const savedImage2 = localStorage.getItem("courseImage2");
+    const savedImage3 = localStorage.getItem("courseImage3");
+    const savedImage4 = localStorage.getItem("courseImage4");
 
     title.innerText = savedTitle;
     banner.src = savedBanner;
     desc.innerHTML = savedDesc;
     sylabus.innerHTML = savedSylabus;
     total.innerHTML = savedTotal;
+    maxPoint.innerHTML = savedMaxPoint;
+    point.innerHTML = savedPoint;
+    score.innerHTML = savedScore;
+    image1.src = savedImage1;
+    image2.src = savedImage2;
+    image3.src = savedImage3;
+    image4.src = savedImage4;
 }
